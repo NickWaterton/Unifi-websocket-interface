922ef386e804b15629f03322eeab8eafe45fc96b
Bryce Lamborne <blamb@live.com.au>
2021-08-08 20:03:06 +1000

    created setup to generate whl

---
+++ pyunifiwsi/__init__.py
@@ -0,0 +1,1 @@
+from .unifi_client import UnifiClient
---
+++ setup.py
@@ -0,0 +1,37 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.1.0'
+PACKAGE_NAME = 'pyunifiwsi'
+AUTHOR = 'You'
+AUTHOR_EMAIL = 'you@email.com'
+URL = 'https://github.com/NickWaterton/Unifi-websocket-interface'
+
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'Describe your package in one sentence'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'numpy',
+      'pandas',
+      'requests',
+      'hjson',
+      'bs4',
+      
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
